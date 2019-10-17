500b2de154bebbd61c0f74f417b0b8c999e7aabe
Tomas Sramek <tsramek@deloittece.com>
2019-10-17 14:20:06 +0200

    Arrays completed

--- app/arrays.js
+++ app/arrays.js
@@ -28,7 +28,6 @@ exports.arraysAnswers = {
   },
 
   remove: function(arr, item) {
-
     for (let index = 0; index < arr.length; index++) {
       let el = arr[index]
 
@@ -42,50 +41,117 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy: function(arr, item) {
-    console.log(arr, item)
-
     for (let index = 0; index < arr.length; index++) {
-      
+      let el = arr[index]
+
+      if (el === item) {
+        arr.splice(index, 1)
+        index--;
+      }
     }
+
+    return arr
   },
 
   append: function(arr, item) {
+    arr.push(item)
 
+    return arr
   },
 
   truncate: function(arr) {
+    arr.pop()
+
+    return arr
 
   },
 
   prepend: function(arr, item) {
+    arr.unshift(item)
 
+    return arr
   },
 
   curtail: function(arr) {
+    arr.shift()
 
+    return arr
   },
 
   concat: function(arr1, arr2) {
+    let concatenatedArr = arr1.concat(arr2);
 
+    return concatenatedArr
   },
 
   insert: function(arr, item, index) {
+    arr.splice(index, 0, item)
+
+    return arr
 
   },
 
   count: function(arr, item) {
+    let count = 0;
 
+    for (let index = 0; index < arr.length; index++) {
+      let el = arr[index]
+
+      if (el === item) count++;
+    }
+
+    return count
   },
 
-  duplicates: function(arr) {
+  duplicates: function(arr) { // 1, 3, 4
+    //console.log(arr)
+    var object = {};
+    var result = [];
 
+    arr.forEach(function (item) {
+      // check if item exists in the object array already
+      // if it is a first occurence in the array, assign it value = 0
+      if(!object[item]) object[item] = 0;
+          //console.log(object[item])
+          // OTHERWISE count how many duplicates it has
+          object[item] += 1;
+    })
+
+    //console.log(object)
+
+    for (var prop in object) {
+      //console.log(prop)
+        if(object[prop] >= 2) {
+            result.push(prop);
+        }
+    }
+    
+    resultArr = Array.from(Object.keys(result).map(i => parseInt(result[i])))
+
+    return resultArr;
   },
 
   square: function(arr) {
+    for (let index = 0; index < arr.length; index++) {
+      let itemSquare = arr[index] * arr[index];
 
+      arr.splice(index, 1, itemSquare)
+    }
+
+    return arr
   },
 
   findAllOccurrences: function(arr, target) {
+    let occurArr = new Array;
 
+    for (let index = 0; index < arr.length; index++) {
+      let el = arr[index]
+
+      if (el === target) {
+        occurArr.push(index)
+      }
+    }
+
+    return occurArr
   }
 };
