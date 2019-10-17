d6916b7316d23742d0f851bd313f92f73f7eeaff
Tomas Sramek <tsramek@deloittece.com>
2019-10-17 09:46:20 +0200

    Initial commit

--- app/arrays.js
+++ app/arrays.js
@@ -2,19 +2,51 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+    //console.log(item);
+    for (let index = 0; index < arr.length; index++) {
+      let element = arr[index];
+      // console.log(element,index,item);
+      // console.log(element === item);
+      
+      if (element === item) {
+        return index;
+      }
+    }
 
+    return -1;
   },
 
   sum: function(arr) {
+    let summary = 0;
+
+    for (let index = 0; index < arr.length; index++) {
+      summary += arr[index];
+    }
+
+    return summary;
 
   },
 
   remove: function(arr, item) {
 
+    for (let index = 0; index < arr.length; index++) {
+      let el = arr[index]
+
+      if (el === item) {
+        arr.splice(index, 1)
+        index--;
+      }
+    }
+
+    return arr;
   },
 
   removeWithoutCopy: function(arr, item) {
+    console.log(arr, item)
 
+    for (let index = 0; index < arr.length; index++) {
+      
+    }
   },
 
   append: function(arr, item) {
